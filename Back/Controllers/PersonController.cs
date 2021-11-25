88afa5b6129ac70ea0d83a2d84c6e90e57f9849e
ElisShrif <dashast30@gmail.com>
2021-11-26 01:30:19 +0500

    Добавила контроллер Interst и поля в person

--- Back/Controllers/PersonController.cs
+++ Back/Controllers/PersonController.cs
@@ -1,5 +1,6 @@
 ﻿using Back.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Back.Controllers
 {
@@ -13,7 +14,12 @@ namespace Back.Controllers
             List<Person> persons;
             using (var db = new AppDBContext())
             {
-                persons = db.Persons.ToList();
+                persons = db.Persons
+                    .Include(x => x.Avatar)
+                    .Include(x => x.Gender)
+                    .Include(x => x.City) // добавить привязку страны, пока чисто город
+                    .Include(x => x.Interests)
+                    .ToList();
             }
             return persons;
         }
@@ -25,6 +31,9 @@ namespace Back.Controllers
             using (var db = new AppDBContext())
             {
                 person = db.Persons.Find(id);
+
+                db.Genders.Where(c => c.Id == person.GenderId).Load();
+                db.Cities.Where(c => c.Id == person.CityId).Load();
             }
             if (person == null)
                 return NotFound();
@@ -40,6 +49,9 @@ namespace Back.Controllers
             {
                 person = db.Persons.Add(item).Entity;
                 db.SaveChanges();
+
+                db.Genders.Where(c => c.Id == person.GenderId).Load();
+                db.Cities.Where(c => c.Id == person.CityId).Load();
             }
 
             return person;
